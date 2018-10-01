85575f809ae4ef8abdef429da7b30adeefafb3e3
Rakesh Jha <29140@Hexaware.com>
2018-10-01 17:32:31 +0530

    First

--- webhook.js
+++ webhook.js
@@ -1,23 +1,79 @@
+var request=require('request');
+var express = require('express'),
+  app = express(),
+  http = require('http'),
+  httpServer = http.Server(app),
+  
+  session = require('express-session');
+const crypto = require('crypto');
+var router = express.Router();
+// Moment JS
 
-const Sentiment = require('sentiment');
-var express = require('express');
-var session = require('express-session')
+
+
+
 var bodyParser = require('body-parser');
-var requestAPI = require('request');
-
-
-
-
-app = express();
-
-
-var port = process.env.PORT || 5000;
-
-app.use(express.static(__dirname));
+var fs = require('fs');
+const requestAPI = require('request');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+app.use(session({
+  secret: 'login',
+  key: 'opty'
+}));
+app.use(express.static(__dirname));
+// app.use(passport.initialize());
+// app.use(passport.session());
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
 }));
+
+app.post("/webhook",async (req,res)=>{
+  var options = {
+    url: "https://api.dialogflow.com/v1/query?v=20150910",
+    method: "POST",
+    headers: { 'Authorization': 'Bearer ' + '60da65e2782f4c1ab12a48a2f56cdc3d', 'Content-Type': 'application/json'},
+    body: req.body,
+    json: true
+  };
+  await requestAPI(options, function (error, response, body) {
+  console.log('1------------',body);
+    
+      
+             res.send(body);
+    
+  
+  });
+})
+
+var jsonIncompleteTran = [];
+
+app.get('/', function (req, res) {
+  res.send("/richowebsites");
+});
+
+
+app.get('/chatwindow', function (req, res) {
+  readFile("IncompleteTransaction.json", function (hasFile, data) {
+    if (hasFile) {
+      jsonIncompleteTran = data;
+    }
+    res.sendfile(__dirname + '/chatwindow1.html');
+  });
+});
+app.get('/roaming', function (req, res) {
+  readFile("IncompleteTransaction.json", function (hasFile, data) {
+    if (hasFile) {
+      jsonIncompleteTran = data;
+    }
+    res.sendfile(__dirname + '/roaming.html');
+  });
+});
+app.get('/chat', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+
+
 
 app.post("/fulfillment", async function (req, res) {
 
@@ -132,6 +188,12 @@ app.post("/fulfillment", async function (req, res) {
 //POST Call Endpoint
 
 
-app.listen(port);
 
 
+app.listen(process.env.PORT || 9000);
+
+
+
+
+
+   
