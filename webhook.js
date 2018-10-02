33cdb80851910116a38f9b19d1e3e8dee859d5b4
rakesh031986jha <rakesh031986jha@gmail.com>
2018-10-02 20:30:25 +0530

    Call fun

--- webhook.js
+++ webhook.js
@@ -360,7 +360,7 @@ function priceConverter(req,res){
   glassType
   windowType
   console.log((gSize&&glassType&&windowType!==null));
-  if(gSize&&glassType&&windowType){
+  if(gSize&&glassType&&windowType!==null){
   var options = { method: 'POST',
   url: 'http://35.154.116.87:7999/aa/getMockGlassCost',
   headers: 
