37862e3b788345e79822efe00f3e0902a600ccdd
Rakesh Jha <29140@Hexaware.com>
2018-10-02 10:39:15 +0530

    First

--- webhook.js
+++ webhook.js
@@ -328,11 +328,9 @@ function priceConverter(req,res){
 
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
-  console.log(body);
-  var price = body.result;
-  console.log("come in side or not");
-  console.log(error);
-  console.log(response);
+  
+  var price = body.glassCost;
+  
   return price;
       
 });
