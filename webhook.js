91458807397428f4a9a5a2a7d897833802089a93
jha143self <jha143@gmail.com>
2018-10-03 16:05:12 +0530

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -73,22 +73,114 @@ app.get('/chat', function (req, res) {
 });
 
 
-var claimno;
-var lossDate;
-var lossType;
-var lossCause;
-var description;
-var price;
-var height;
-var width;
-var thickness;
-var glassType;
-var windowType;
-var gSize;
+var claimno;
+var lossDate;
+var lossType;
+var lossCause;
+var description;
+var price;
+var height;
+var width;
+var thickness;
+var glassType;
+var windowType;
+var gSize; 
 
+async function priceConverter(req,res){
+  console.log("glassType: "+glassType+"windowType: "+windowType)
+  var options = { method: 'POST',
+  url: 'http://35.154.116.87:7999/aa/getMockGlassCost',
+  headers: 
+   { 'postman-token': '225193bc-ade0-bb34-6a7e-b6e8851b7c3b',
+     'cache-control': 'no-cache',
+     'content-type': 'application/json' },
+  body: 
+   { height: 70,
+     width: 30,
+     thickness: 33,
+     glassType: glassType,
+     windowType: windowType},
+  json: true };
+return new Promise((resolve, reject) => {
+requestAPI(options, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    
+    var validprice= body;
+    price=roundTo(validprice.glassCost, 2);
+    console.log(price); 
+     return resolve(price);
+     } else {
+     return reject(error);
+    }    
+  
+ 
+});
+  }
+)
+
+  
+}
+async function CreateClaim(req,res)
+{
+ console.log('inside create claim------------');
+  var options = { method: 'POST',
+     
+  url: 'http://35.154.116.87:8080/cc/service/edge/fnol/cfnol',
+
+  headers:
+
+   { 'postman-token': 'ff149a5b-daaf-0000-0b8c-5301c162be75',
+
+     'cache-control': 'no-cache',
+
+     authorization: 'Basic c3U6Z3c=',
+
+     accept: 'application/json',
+
+     'content-type': 'application/json' },
+
+  body:
+
+   { jsonrpc: '2.0',
+
+     method: 'createClaimForHomeOwners',
+
+     params:
+
+     [ { lossDate: lossDate+'T00:00:00Z',
+     
+               lossType: 'PR',
+     
+               lossCause: 'glassbreakage',
+     
+               description: 'windowcrashed' } ] },
+
+  json: true };
+
+ 
+return new Promise((resolve, reject) => {
+  console.log("Inside promise") ;
+  //return resolve("000-00-000255");
+ 
+requestAPI(options, function (error, response, body) {
+  console.log('2------------',body);
+ 
+
+  if (!error && response.statusCode === 200) {
+   
+    claimno = body.result;
+      console.log(claimno); 
+    return resolve(claimno);
+    } else {
+    return reject(error);
+   }    
+      
+});
+});
+}
 app.post("/fulfillment", async function (req, res) {
 
-
+  // Service now status
   
   console.log("body", JSON.stringify(req.body));
   console.log(JSON.stringify(req.body.result.action));
@@ -102,10 +194,11 @@ app.post("/fulfillment", async function (req, res) {
   var type = null;
   var smsType = null;
   var smsContent = '';
-  
+  var resp = commonFiles.WelcomeMsg();
   var msg = '';
+ 
   
-  
+  // Input.welcome -----------
   if (intentFrom === 'input.welcome' ) {
     msg = {
       "speech": "",
@@ -212,18 +305,130 @@ app.post("/fulfillment", async function (req, res) {
     return res.json(msg);
   }
 
-  else if(intentFrom === 'input.damaged') {
-    msg = {"speech": "Hold on for a moment while we get the details of the damaged glass",
-    "messages": [
+  else if(intentFrom === 'upload_image') {
+   
+    windowType=intentParam.WindowType;
+    console.log("#####"+windowType);
+   
        
+        
+    msg = {
+    "messages": [
+      {
+      "type": 0,
+      "platform": "facebook",
+      "speech": "Hold on for a moment while we get the details of the damaged glass"
+      },
         {
-          "type": 2,
+          "type": 4,
           "platform": "facebook",
-          "title": "Can you validate the type of window? You can select another one if the suggested window type is not correct",
-          "replies": [
-            "Single Hung"
-          ]
-        }
+          "payload":{
+          "facebook":{
+          "text": "Can you validate the type of window? You can select another one if the suggested window type is not correct",
+          "quick_replies": [
+            {
+                          "content_type":"text",
+                          "title":"Single Hung Windows",
+                          "payload":"Single Hung Windows",
+                          "selected":true
+              },
+            {
+                          "content_type":"text",
+                          "title":"Double Hung Windows",
+                          "payload":"Double Hung Windows"
+                       
+              },
+            {
+                          "content_type":"text",
+                          "title":"Arched Windows",
+                          "payload":"Arched Windows"
+                       
+              },
+            {
+                          "content_type":"text",
+                          "title":"Bay Windows",
+                          "payload":"Bay Windows",
+                       
+              },
+            {
+                          "content_type":"text",
+                          "title":"Bow Windows",
+                          "payload":"Bow Windows",
+                       
+              },
+            {
+                          "content_type":"text",
+                          "title":"Casement Windows",
+                          "payload":"Casement Windows",
+                       
+              },{
+                          "content_type":"text",
+                          "title":"Egress Windows",
+                          "payload":"Egress Windows",
+                       
+              },{
+                          "content_type":"text",
+                          "title":"Garden Windows",
+                          "payload":"Garden Windows",
+                       
+              },{
+                          "content_type":"text",
+                          "title":"Glass Block Windows",
+                          "payload":"Glass Block Windows",
+                       
+              },{
+                          "content_type":"text",
+                          "title":"Hopper Windows",
+                          "payload":"Hopper Windows",
+                       
+              },
+            {
+                          "content_type":"text",
+                          "title":"Jalousie Windows",
+                          "payload":"Jalousie Windows",
+                       
+              },
+            {
+                          "content_type":"text",
+                          "title":"Picture Windows",
+                          "payload":"Picture Windows",
+                       
+              },
+            {
+                          "content_type":"text",
+                          "title":"Round Circle Windows",
+                          "payload":"Round Circle Windows",
+                       
+              },
+            {
+                          "content_type":"text",
+                          "title":"Skylight Windows",
+                          "payload":"Skylight Windows",
+                       
+              },
+            {
+                          "content_type":"text",
+                          "title":"Sliding Windows",
+                          "payload":"Sliding Windows",
+                       
+              },
+            {
+                          "content_type":"text",
+                          "title":"Storm Windows",
+                          "payload":"Storm Windows",
+                       
+              },
+            {
+                          "content_type":"text",
+                          "title":"Transom Windows",
+                          "payload":"Transom Windows",
+                       
+              }
+                       
+                      ]
+            
+            
+        }}}
       ]
   
       };
@@ -232,28 +437,90 @@ app.post("/fulfillment", async function (req, res) {
  
   
   else if(intentFrom === 'input.windows') {
-
     windowType=intentParam.Windows;
     console.log(windowType);
-    msg = {"speech": "",
+    msg = {
+      "speech": "",
+    "displayText": "",
     "messages": [
            {
-        "type": 2,
+        "type": 4,
         "platform": "facebook",
-        "title": "Can you validate the type of glass? You can select another one if the suggested glass type is not correct",
-        "replies": [
-          "Float Glass"
-        ]
-      }
+        "payload":{
+        "facebook":{
+        "text": "Can you validate the type of glass? You can select another one if the suggested glass type is not correct",  
+        "quick_replies": [
+          {
+                        "content_type":"text",
+                        "title":"Float Glass",
+                        "payload":"Float Glass",
+                        "selected":true
+            },
+          {
+                        "content_type":"text",
+                        "title":"Safety Laminated Glass",
+                        "payload":"Safety Laminated Glass"
+                     
+            },
+          {
+                        "content_type":"text",
+                        "title":"Obscured Glass",
+                        "payload":"Obscured Glass"
+                     
+            },
+          {
+                        "content_type":"text",
+                        "title":"Annealed Glass",
+                        "payload":"Annealed Glass",
+                     
+            },
+          {
+                        "content_type":"text",
+                        "title":"Tinted Glass",
+                        "payload":"Tinted Glass",
+                     
+            },
+          {
+                        "content_type":"text",
+                        "title":"Tempered Glass",
+                        "payload":"Tempered Glass",
+                     
+            },{
+                        "content_type":"text",
+                        "title":"Insulated Glass",
+                        "payload":"Insulated Glass",
+                     
+            },{
+                        "content_type":"text",
+                        "title":"Mirrored Glass",
+                        "payload":"Mirrored Glass",
+                     
+            },{
+                        "content_type":"text",
+                        "title":"Low-E Glass",
+                        "payload":"Low-E Glass",
+                     
+            },{
+                        "content_type":"text",
+                        "title":"Wired Glass",
+                        "payload":"Wired Glass",
+                     
+            },
+          {
+                        "content_type":"text",
+                        "title":"Heat Strengthened Glass",
+                        "payload":"Heat Strengthened Glass",
+                     
+            }]
+      }}}
     ]
 
     }
     return res.json(msg);
   }
-  else if(intentFrom === 'input.Glass') {
+  else if(intentFrom === 'input.sizeOfglass') {
     glassType=intentParam.GlassType;
-    
-    console.log(glassType);
+    console.log(glassType); 
     msg = {
       "speech": "",
       "displayText": "",
@@ -271,34 +538,51 @@ app.post("/fulfillment", async function (req, res) {
   else if(intentFrom === 'input.GlassSize') {
     gSize=intentParam.GlassSize;
     console.log(gSize);
-    console.log(glassType);
-    console.log(windowType);
-    console.log(lossDate);
-    var price={};
-    claimno=CreateClaim(req,res);
-   price=priceConverter(req,res);
-   console.log("claimno Jha"+claimno);
-   console.log("Rakkesh Jha"+price);
-  //if(price!=null){
-   msg={
-      "speech": "",
-       "displayText": "",
-       "messages": [{
-        "type": 0,
-        "platform": "facebook",
-        "speech": "Your Claim number is CL  "+claimno
-      },{
-        "type": 0,
-        "platform": "facebook",
-        "speech": "Based on the quotes received from the market, you are entitled to a claims payment of "+price+
-        ". We've added an additional 10% to the market rates to cover any additional expenses that you may incur. "
-      }]
+         
+    await CreateClaim(req,res).then(async (claimno) => {
       
-    };
+            if(claimno){
+             console.log("Claim Number"+claimno);
+              await priceConverter(req,res).then((price) => {
+                console.log("Claim Number"+price);
+                          msg={
+                            "speech": "",
+                            "displayText": "",
+                             "messages": [{
+                              "type": 0,
+                              "platform": "facebook",
+                              "speech": "Your Claim number is CL  "+claimno
+                            },{
+                              "type": 0,
+                              "platform": "facebook",
+                              "speech": "Based on the quotes received from the market, you are entitled to a claims payment of $ "+price+
+                              ". We've added an additional 10% to the market rates to cover any additional expenses that you may incur. "
+                            }]
+                            
+                          };
+                          return res.json(msg);
+                        });
+                      
+                        
+        }});
+   
+  //if(price!=null){
+   
   
-    return res.json(msg);
+    
   }
-
+  else if(intentFrom === 'otherOption') {
+    msg={
+    "speech": "",
+    "displayText": "",
+    "messages": [{
+    "type": 0,
+    "platform": "facebook",
+    "speech": "Oh Yes.<br><br>We can get the glazing repaired for you <br><br> which would take 2 weeks to complete. <br><br>Alternatively, you can send us a quote from a glazier and we'll pay out the claim"
+    }]
+    };
+    return res.json(msg);
+    } 
   else if(intentFrom === 'input.OtherOptionRes') {
     msg = {
       "speech": "",
@@ -311,7 +595,7 @@ app.post("/fulfillment", async function (req, res) {
           "text":"Please select an option for us to proceed further",
           "quick_replies_img":[{
             "content_type":"text",
-            "title":"Cash Payment of USD",
+            "title":"Cash Payment "+ price,
             "payload":"Cash Payment of USD"
           },{
             "content_type":"text",
@@ -331,91 +615,6 @@ app.post("/fulfillment", async function (req, res) {
   
 });
 //POST Call Endpoint
-
-function CreateClaim(req,res)
-{
-  //console.log('inside create claim------------',req);
-  var options = { method: 'POST',
-     
-  url: 'http://35.154.116.87:8080/cc/service/edge/fnol/cfnol',
-
-  headers:
-
-   { 'postman-token': 'ff149a5b-daaf-0000-0b8c-5301c162be75',
-
-     'cache-control': 'no-cache',
-
-     authorization: 'Basic c3U6Z3c=',
-
-     accept: 'application/json',
-
-     'content-type': 'application/json' },
-
-  body:
-
-   { jsonrpc: '2.0',
-
-     method: 'createClaimForHomeOwners',
-
-     params:
-
-      [ { lossDate: '2018-09-27T00:00:00Z',
-
-          lossType: 'PR',
-
-          lossCause: 'glassbreakage',
-
-          description: 'windowcrashed' } ] },
-
-  json: true };
-
- 
-
-request(options, function (error, response, body) {
-  console.log('2------------',body);
-  if (error) throw new Error(error);
-console.log("Rakesh jha");
-claimno = body.result;
-  console.log(claimno);
-  
-  console.log('3------------',claimno);
-          
-      
-});
-return claimno;
-}
-
-function priceConverter(req,res){
-  gSize
-  glassType
-  windowType
-  console.log((gSize!==null+"rak"+glassType!==null+"dkjasd"+windowType!==null));
-  if(gSize!==null && glassType!==null && windowType!==null){
-  var options = { method: 'POST',
-  url: 'http://35.154.116.87:7999/aa/getMockGlassCost',
-  headers: 
-   { 'postman-token': '225193bc-ade0-bb34-6a7e-b6e8851b7c3b',
-     'cache-control': 'no-cache',
-     'content-type': 'application/json' },
-  body: 
-   { height: 70,
-     width: 30,
-     thickness: 33,
-     glassType: glassType,
-     windowType: windowType },
-  json: true };
-
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-  var validprice= body;
-  price=validprice.glassCost
-  console.log(validprice.glassCost);
-});
-  }
-
-return price;
-  
-}
 
 
 app.listen(process.env.PORT || 9000);
